0019f6d774a31e71b3a7e18294513944363e8d08
tmgAzit <azit.ghising@gmail.com>
2022-05-20 14:57:38 +0930

    pages and components with global context are define.

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, dolor aut
+        laudantium at sed iusto inventore. Cum dolorem dolor dignissimos totam
+        ut. Qui harum enim unde doloribus suscipit facilis nihil temporibus ea.
+        Modi neque dolorum corporis voluptate laudantium ea molestias
+        perspiciatis praesentium ipsa magni laboriosam, officiis a aut tempore
+        repellendus?
+      </p>
+    </section>
+  );
+};
+
+export default About;
